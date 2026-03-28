93cb3e09d5952c6e2734543459d7b63e689b75b3
nayhlaing109-maker <nayhlaing109@gmail.com>
2026-03-28 14:46:45 +0630

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,11 +3,11 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 
 // 🔥 သင်၏ Firebase Project Settings ထဲက Config ကုဒ်လေးတွေနဲ့ အစားထိုးပါ
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD3hhkLybww1LCS8ir2EU7nDxrO7rOAoP0",
+  authDomain: "pyapay.firebaseapp.com",
+  projectId: "pyapay",
+  messagingSenderId: "369757698666",
+  appId: "1:369757698666:web:6e30f7c55fd220b4cc2813",
 });
 
 const messaging = firebase.messaging();
