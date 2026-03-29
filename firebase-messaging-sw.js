7d62c0490d136f40785ca244d70b8ee4cd40187e
nayhlaing109-maker <nayhlaing109@gmail.com>
2026-03-29 09:36:06 +0630

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,24 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-// 🔔 ပုံ (fdf523ec) ထဲက သင်၏ Web Config Key များ
 const firebaseConfig = {
-  apiKey: "AIzaSyD3hhkLybww1LCS8ir2EU7nDxrO7rOAoP0",
-  authDomain: "pyapay.firebaseapp.com",
-  projectId: "pyapay",
-  storageBucket: "pyapay.firebasestorage.app",
-  messagingSenderId: "369757698666",
-  appId: "1:369757698666:web:6e30f7c55fd220b4cc2813",
-  measurementId: "G-F85J6C4B3P"
+    apiKey: "AIzaSyB3oEgakHXj4NOos0QO61-HOuM165VOVYI",
+    authDomain: "pyapay-driver.firebaseapp.com",
+    projectId: "pyapay-driver",
+    storageBucket: "pyapay-driver.firebasestorage.app",
+    messagingSenderId: "719139320259",
+    appId: "1:719139320259:web:2e81dc869c0358135bcd76"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Background မှာ Notification ပြသရန်
+// နောက်ကွယ်မှာ message လက်ခံဖို့
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/firebase-logo.png'
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/favicon.ico' 
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
