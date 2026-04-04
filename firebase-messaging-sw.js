8d5b9a084654edbe5cd35832bc9a1de409c0ae10
nayhlaing109-maker <nayhlaing109@gmail.com>
2026-04-04 14:15:43 +0630

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js');
 
